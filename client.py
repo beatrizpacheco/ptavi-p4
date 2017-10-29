fb4ee591c960dae15a90385384182c3f0c2db1bc
bea <b.pacheco@alumnos.urjc.es>
2017-10-29 10:40:40 +0100

    pep 8 pasado en cliente y servidor

--- client.py
+++ client.py
@@ -9,7 +9,8 @@ import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
 if len(sys.argv) < 6:
-    sys.exit("Usage: python3 client.py ip puerto register sip_address expires_value")
+    sys.exit('Usage: python3 client.py ip puerto'
+             'register sip_address expires_value')
 
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
@@ -22,7 +23,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", USER)
     if sys.argv[3] == 'register':
-        my_socket.send(bytes('REGISTER sip:'+USER+' SIP/2.0\r\nExpires: '+EXPIRES+'\r\n\r\n', 'utf-8') + b'\r\n')
+        my_socket.send(bytes('REGISTER sip:' + USER + ' SIP/2.0\r\nExpires: '
+                             + EXPIRES + '\r\n\r\n', 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
--- server.py
+++ server.py
@@ -8,36 +8,35 @@ import json
 import socketserver
 import sys
 import time
-from time import gmtime , strftime
+from time import gmtime, strftime
 
 if len(sys.argv) < 2:
     sys.exit("Usage: python3 server.py puerto")
-
 PORT = int(sys.argv[1])
+
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
     dic = {}
-    
+
     def json2registered(self):
-        with open('registered.json', 'r') as fich:
-            for linea in fich:
-                print('holaaaaaa' +linea)
-                self.dic = json.loads(linea)
-                print('estoy en json2...' + str(self.dic))
-                #Lo coge bien del fichero, pero los demas def no lo usan bien
-    
+        try:
+            with open('registered.json', 'r') as fich:
+                self.dic = json.load(fich)
+        except:
+            pass
+
     def register2json(self):
         json.dump(self.dic, open('registered.json', "w"))
-    
+
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
-        print('estoy en handle' + str(self.dic))
+        self.json2registered()
         for line in self.rfile:
             message = line.decode('utf-8').split()
             if message:
@@ -46,7 +45,9 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                     info = self.client_address[0]
                 if message[0] == 'Expires:':
                     if message[1] != '0':
-                        Expire = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()+int(message[1])))
+                        Expire = time.strftime('%Y-%m-%d %H:%M:%S',
+                                               time.gmtime(time.time() +
+                                                           int(message[1])))
                         self.dic[user] = [info, Expire]
                         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                     elif message[1] == '0':
@@ -54,18 +55,17 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                             del self.dic[user]
                         except KeyError:
-                            self.wfile.write(b"SIP/2.0 404 User Not Found\r\n\r\n")
+                            self.wfile.write(b'SIP/2.0 404 User'
+                                             b'Not Found\r\n\r\n')
             print(line.decode('utf-8'), end='')
         print(self.dic)
         self.register2json()
 
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) #'' es localhost
-    SIPRegisterHandler.json2registered(serv)
-    print('estoy en el main' + str(serv.dic))
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
-        serv.serve_forever() #Esperando alguna conexion infinitamente
+        serv.serve_forever()  # Esperando alguna conexion infinitamente
     except KeyboardInterrupt:
         print("Finalizado servidor")
